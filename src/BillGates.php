9ce3b219b46e1a3824afc774012031e8c72acf4c
Jesse Jusoli <jessejusoli@gmail.com>
2024-03-24 17:42:26 -0300

    Update BillGates.php
    Adicione comentários ao código para melhor clareza:

--- src/BillGates.php
+++ src/BillGates.php
@@ -2,8 +2,16 @@
 
 namespace JesseJusoli\BillGates;
 
+use Symfony\Component\Process\Process;
+
 class BillGates
 {
+    /**
+     * Atualiza as dependências do Composer, garantindo que seja executada no máximo uma vez por dia.
+     *
+     * @return bool True se a atualização foi realizada com sucesso, false se não foi necessário atualizar ou houve um erro.
+     * @throws \RuntimeException Se houver um erro durante a atualização das dependências.
+     */
     public static function updateDependencies()
     {
         $lastUpdateFile = __DIR__ . '/../last_update.txt';
@@ -13,18 +21,18 @@ class BillGates
         if ($lastUpdate === null || strtotime($lastUpdate) < strtotime('-1 day')) {
             $composerPath = realpath(__DIR__ . '/../../composer.json');
             $composerPath = escapeshellarg($composerPath); // Escapa o caminho para evitar injeção de comandos maliciosos
-            $command = 'composer update -d ' . dirname($composerPath);
 
-            // Executa o comando e captura a saída e o código de retorno
-            exec($command, $output, $returnCode);
+            // Cria um novo processo para executar o comando Composer
+            $process = new Process(['composer', 'update', '-d', dirname($composerPath)]);
+            $process->run();
 
             // Verifica se a execução do comando foi bem-sucedida
-            if ($returnCode === 0) {
+            if ($process->isSuccessful()) {
                 // Atualiza a data da última atualização
                 file_put_contents($lastUpdateFile, date('Y-m-d H:i:s'));
                 return true; // Indica que a atualização foi bem-sucedida
             } else {
-                return false; // Indica que houve um erro durante a atualização
+                throw new \RuntimeException('Erro ao atualizar as dependências do Composer: '.$process->getErrorOutput());
             }
         } else {
             // A atualização já foi realizada nas últimas 24 horas
